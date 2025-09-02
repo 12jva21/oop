f10ff8a06da128ebb94348f0b0d99f1bbed2054c
n0t_c0mp13x <jairovegaanacona1221@gmail.com>
2025-09-02 11:02:53 -0500

    Update tarea1.py
    se añadio un algoritmo capaz de detectar numeros primos usando dos ciclos for anidados

--- POO/tarea1.py
+++ POO/tarea1.py
@@ -4,5 +4,13 @@
 #Encontrar los numeros primos entre cero y 50
 #El operador clave es el modulo
 
+contador=0
+
+for i in range (2,50):
+    for k in range (1,i+1):
+        if(i%k == 0):
+            contador+=1
+    if(contador==2):
+        print(i)
 
 
