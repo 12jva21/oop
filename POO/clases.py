3c49452e5e325b935eb57d5949c5816f59d487fe
n0t_c0mp13x <jairovegaanacona1221@gmail.com>
2025-09-09 15:33:55 -0500

    Add files via upload

---
+++ POO/clases.py
@@ -0,0 +1,43 @@
+'''
+Abstraer elementos
+Esquema modular y diagrama de clases
+Los requerimientos son el contexto. El levantamiento de requerimientos es identificar el tipo de variables que necesitan los atributos.
+Los atributos son las figuras a las que aplican las acciones
+Los metodos son las acciones
+Los objetos son el producto de uno o varios atributos, metodos y clases
+Una instancia es darle los valores a las variables de los objetos
+Hay tipo de valor objeto
+El constructor es el metodo que crea los atributos de los objetos de la clase( __init__- self)
+getters setters para obtener y cambiar los atributos
+'''
+
+#Autor: Jairo vega Anacona
+#Motivo: Tarea 2
+
+
+#implementar dos clases una de perro y otra de gato, cuyos objetos instanciados, puedan ladrar y maullar. Los datos de los animales deben ser ingresados por consola
+
+print("Hola! vamos a crear a sus mascotas")
+
+class perro:
+    def __init__(self,razaP,colorP):
+        self.razaP = input("Ingrese la raza de su perro: ")
+        self.colorP = input("Ingrese el color de su perro: ")
+
+    def ladrar(self):
+        print(f"Su perro {self.colorP} de raza {self.razaP} esta ladrando! ")
+
+
+class gato:
+    def __init__(self,razaG,colorG):
+        self.razaG = input("Ingrese la raza de su gato: ")
+        self.colorG = input("Ingrese el color de su gato: ")
+
+    def maullar(self):
+        print(f"Su gato {self.colorG} de raza {self.razaG} esta maullando! ")
+
+miPerro = perro()
+miPerro.ladrar()
+
+miGato = gato ()
+miGato.maullar 
